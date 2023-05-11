45049b60cc708b32f877e25b001a70785b85d629
moolraaj <116911577+moolraaj@users.noreply.github.com>
2023-05-11 13:35:32 +0530

    email added in first

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ function App() {
     <>
     <h1>welocme</h1>
     <p>this is paragraph</p>
-<<<<<<< HEAD
-    <h1>hii this is second</h1>
-=======
     <h1>hii this is second branch</h1>
->>>>>>> a025db7b46a58c6c3616f0954da5feee0f9f2da7
+    <h1>raaj73906@gmail.com</h1>
      
      
     </>
